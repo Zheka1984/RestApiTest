47a5f7831bf6620c23a0ff11793ce0c77aa144bc
Zheka1984 <svea2@yandex.ru>
2023-04-10 18:21:49 +0300

    new

--- src/test/java/Tests/TestClass.java
+++ src/test/java/Tests/TestClass.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import dataObjects.MainData;
